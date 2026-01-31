a13e8fd17d06cdec65985fd82fd82341ee7ff03c
Gabe Fernandez <gabecfernandez@gmail.com>
2026-01-31 09:11:24 -0600

    Initial Commit

---
+++ AI4Science/AI4ScienceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI4ScienceApp.swift
+//  AI4Science
+//
+//  Created by Gabe Fernandez on 1/31/26.
+//
+
+import SwiftUI
+
+@main
+struct AI4ScienceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
